12a7f66076560c9e34e1a6731e03a03314443c04
DEV_NJH <mark0112530@gmail.com>
2021-10-17 21:57:32 +0900

    Delete downloadbutton.js

--- assets/js/downloadbutton.js
+++
@@ -1,8 +0,0 @@
-function btn_active(){
-document.querySelector('.texto_centro').className = "texto_centro active_txt";
-  document.querySelector('.cont_centrar').className = "cont_centrar activebtn";
-  setTimeout(function(){
-      document.querySelector('.cont_centrar').className = "cont_centrar activebtn_fin";
-    document.querySelector('.texto_centro').className = "texto_centro op_0";
-  },15000);
-}
